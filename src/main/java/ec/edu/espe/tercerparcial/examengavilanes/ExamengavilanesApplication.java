2b2b2ec018abe4d4afef5b63b8160250fad8a08e
Jacqueline <65547052+jigavilanes1@users.noreply.github.com>
2024-03-11 19:28:18 -0500

    Add files via upload

---
+++ src/main/java/ec/edu/espe/tercerparcial/examengavilanes/ExamengavilanesApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.tercerparcial.examengavilanes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamengavilanesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamengavilanesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/tercerparcial/examengavilanes/ExamengavilanesApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.tercerparcial.examengavilanes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamengavilanesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
